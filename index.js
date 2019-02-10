9abc52285881c706ff74e1faa4ab239edb6d5bd5
goenning <me@goenning.net>
2019-02-10 16:33:16 +0000

    better network control

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const os = require('os');
 const origin = process.env.ORIGIN || os.hostname();
 const requestURL = process.env.REQUEST_URL || 'https://example.org';
 const mongoURL = process.env.MONGO_URL;
+const networkWait = 500;
 
 (async () => {
   const timing = {};
@@ -18,15 +19,17 @@ const mongoURL = process.env.MONGO_URL;
   });
   const page = await browser.newPage();
   timing.start = new Date();
-  await page.goto(requestURL, { waitUntil: 'networkidle0' });
-  timing.end = new Date();
+  await Promise.all([
+    page.goto(requestURL),
+    waitForNetworkIdle(page, networkWait),
+  ]);
+
+  timing.start_ts = timing.start.getTime();
+  timing.duration = new Date().getTime() - timing.start_ts - networkWait;
   timing.origin = origin;
   timing.request_url = requestURL;
   timing.metrics = await page.metrics();
   timing.entries = JSON.parse(await page.evaluate(() => JSON.stringify(performance.getEntries(), null, "  ")));
-  timing.start_ts = timing.start.getTime();
-  timing.end_ts = timing.end.getTime();
-  timing.duration = timing.end_ts - timing.start_ts;
   await browser.close();
 
   if (mongoURL) {
@@ -44,5 +47,38 @@ const store = async (timing) => {
     client.close();
   } catch (err) {
     console.log(err.stack);
+  }
+}
+
+const waitForNetworkIdle = (page, timeout, maxInflightRequests = 0) => {
+  page.on('request', onRequestStarted);
+  page.on('requestfinished', onRequestFinished);
+  page.on('requestfailed', onRequestFinished);
+
+  let inflight = 0;
+  let fulfill;
+  let promise = new Promise(x => fulfill = x);
+  let timeoutId = setTimeout(onTimeoutDone, timeout);
+  return promise;
+
+  function onTimeoutDone() {
+    page.removeListener('request', onRequestStarted);
+    page.removeListener('requestfinished', onRequestFinished);
+    page.removeListener('requestfailed', onRequestFinished);
+    fulfill();
+  }
+
+  function onRequestStarted() {
+    ++inflight;
+    if (inflight > maxInflightRequests)
+      clearTimeout(timeoutId);
+  }
+  
+  function onRequestFinished() {
+    if (inflight === 0)
+      return;
+    --inflight;
+    if (inflight === maxInflightRequests)
+      timeoutId = setTimeout(onTimeoutDone, timeout);
   }
 }
